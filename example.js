45b5072dca82c23701d3c7c0c21d3bba14c70d68
Louis Perrin <louis.perrin@gmail.com>
2014-03-19 20:51:06 +0100

    Update example.js
    example simplification.

--- example.js
+++ example.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-	$(".item15").connect($(".item21"), {x:0, y:0}, {x:0, y:0}, {'border-top':'10px solid red'});
-	$(".item15").connect($(".item11"), {x:0, y:0}, {x:0, y:0}, {'border-top':'10px solid green'});
-	$(".item25").connect($(".item11"), {x:0, y:0}, {x:0, y:0}, {'border-top':'10px solid red'});
-	$(".item25").connect($(".item21"), {x:0, y:0}, {x:0, y:0}, {'border-top':'10px solid green'});
-	$(".item11").connect($(".item25"), {x:0, y:0}, {x:0, y:0}, {'border-top':'10px solid red'});
-	$(".item15").connect($(".item21"), {x:0, y:0}, {x:0, y:0}, {'border-top':'10px solid green'});
-	
-	$(".box1").connect($(".box2"), {x:0, y:0}, {x:0, y:0}, {'border-top':'5px solid blue'});
-	$(".box2").connect($(".box3"), {x:$(".box2").realWidth()/2, y:0}, {x:-$(".box3").realWidth()/2, y:0}, {'border-top':'5px solid orange'});
-	$(".box2").connect($(".box4"), {x:0, y:$(".box2").realHeight()/2}, {x:-$(".box4").realWidth()/2, y:0}, {'border-top':'5px solid pink'});
+	$(".item25").connect($(".box").not(".multibox"), {"border":"solid 2px #f00"}, 'direct', 0);
+	$(".item11").connect($(".item23"), {"border":"solid 2px #0f0"}, 'indirect', 0.5);
+	$(".item12").connect($(".item24"), {"border":"solid 2px #0f0"}, 'indirect', 0.49);
 });
